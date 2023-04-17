36b51fb42fd583f6605e46419ca7a42b93d7ad48
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-04-17 15:39:02 +0300

    b53

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -1,63 +1,68 @@
 // Copyright 2022 NNTU-CS
 #ifndef INCLUDE_TPQUEUE_H_
 #define INCLUDE_TPQUEUE_H_
+
 #include <cassert>
+
 template<typename T, int size>
 class TPQueue {
  private:
   T* box;
-  int capacity, int left, right, int cSize;
- 
+  int currSize;
+  int capacity;
+  int first, last;
+
  public:
-  TPQueue() :capacity(size), left(0), right(0), cSize(0) {
+  TPQueue() :capacity(size), first(0), last(0), currSize(0) {
     box = new T[capacity + 1];
   }
   void push(const T& value) {
-    assert(cSize < capacity);
-    if (cSize == 0) {
-      box[right++] = value;
-      cSize++;
+    assert(currSize < capacity);
+    if (currSize == 0) {
+      box[last++] = value;
+      currSize++;
     } else {
-      int i = right - 1;
-      bool chek = 0;
-      while (i >= left && value.prior > box[i].prior) {
-        chek = 1;
+      int i = last - 1;
+      bool f = 0;
+      while (i >= first && value.prior > container[i].prior) {
+        f = 1;
         box[i + 1] = box[i];
         box[i] = value;
         i--;
       }
-      if (chek == 0) {
-        box[right] = value;
+      if (f == 0) {
+        box[last] = value;
       }
-      right++;
-      cSize++;
+      last++;
+      currSize++;
     }
-    if (right > capacity) {
-      right -= capacity + 1;
+    if (last > capacity) {
+      last -= capacity + 1;
     }
   }
   const T& pop() {
-    assert(cSize > 0);
-    cSize--;
-    if (left > capacity) {
-      left -= capacity + 1;
+    assert(currSize > 0);
+    currSize--;
+    if (first > capacity) {
+      first -= capacity + 1;
     }
-    return box[left++];
+    return box[first++];
   }
   char get() {
-    assert(cSize > 0);
-    return box[left].ch;
+    assert(currSize > 0);
+    return box[first].ch;
   }
   bool isFull() const {
-    return cSize == capacity;
+    return currSize == capacity;
   }
   bool isEmpty() const {
-    return cSize == 0;
+    return currSize == 0;
   }
   ~TPQueue() {
     delete[] box;
   }
 };
+
 struct SYM {
   char ch;
   int prior;
