2bff148d4ac0dec056557defa51fcc67892f942a
BoroninArtem <125183897+BoroninArtem@users.noreply.github.com>
2023-04-17 15:13:52 +0300

    b53

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -5,37 +5,38 @@
 template<typename T, int size>
 template<typename T, int size = 0>
 class TPQueue {
-private:
-    T mas[size] = { 0 };
-    int head, tail, count;
-public:
-    TPQueue() : head(0), tail(0), count(0) {}
+  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+    T mas[size] = {0};
+    int left, right, count;
+
+ public:
+    TPQueue(): left(0), right(0), count(0) {}
     void push(const T& sm) {
         if (count == 0) {
             mas[0] = sm;
-            ++tail;
+            ++right;
             ++count;
-        }
-        else {
-            for (int i = tail; i > head; --i) {
+        } else {
+            for (int i = right; i > left; --i) {
                 if (mas[(i - 1) % size].prior >= sm.prior) {
                     mas[i % size] = sm;
                     break;
-                }
-                else {
+                } else {
                     mas[i % size] = mas[(i - 1) % size];
-                    mas[(i - 1) % size] = sm;
+                    mas[(i - 1)%size] = sm;
                 }
             }
-            ++tail;
+            ++right;
         }
     }
     const T& pop() {
-        return mas[(head++ % size)];
+        return mas[(left++ % size)];
     }
 };
+
 struct SYM {
-    char ch;
-    int prior;
+  char ch;
+  int prior;
 };
-#endif
+#endif  // INCLUDE_TPQUEUE_H_
